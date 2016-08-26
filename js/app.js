af3ffd52089907e3e6629809b5d1c0eb7449c1ef
jessabean <jessica.avison@gmail.com>
2016-08-26 10:31:22 -0400

    Fix the one second delay at the start of countdown

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
-var container = document.getElementById('timer-wrap');
-var start = 59;
-container.innerHTML = "00";
+var container = document.getElementById('timer-wrap'),
+    start = 60;
+    
+container.innerHTML = '00';
 
 function countDown(i) {
+  i = start - 1;
+
   var int = setInterval(function () {
     container.innerHTML = i;
-    i-- || reset();
+    i-- || loop();
   }, 1000);
 
-  var reset = function() {
+  var loop = function() {
     clearInterval(int);
-    container.innerHTML = "00";
+    container.innerHTML = '00';
     countDown(start);
-  }
+  };
 }
 
 countDown(start);
