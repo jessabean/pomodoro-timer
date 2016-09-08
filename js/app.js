daf9935f6f72ebc3a103fdcd898f54191ca7926a
jessabean <jessica.avison@gmail.com>
2016-09-08 13:59:29 -0400

    Put interval in named function to be called on click

--- js/app.js
+++ js/app.js
@@ -1,38 +1,42 @@
 var minutesWrap = document.getElementById('minutes-wrap'),
     secondsWrap = document.getElementById('seconds-wrap'),
+    startBtn    = document.getElementById('start-button'),
     minutes     = 25,
     seconds = minutes * 60;
+
+// single digits should have leading zero
+var padZero = function(num) {
+  return (num < 10) ? "0"+num : num;
+};
+
+var startTimer = function() {
+  interval = setInterval(countDown, 1000);
+};
+
+var stopTimer = function() {
+  minutesWrap.innerHTML = padZero(minutes);
+  secondsWrap.innerHTML = '00';
+  clearInterval(interval);
+};
+
+var countDown = function() {
+  seconds--;
+
+  if(seconds % 60 === 59) {
+    minutes--;
+    minutesWrap.innerHTML = padZero(minutes);
+  }
+
+  if(seconds === 0) {
+    stopTimer();
+  }
+
+  secondsWrap.innerHTML = padZero(seconds % 60);
+};
 
 // set up display
 minutesWrap.innerHTML = padZero(minutes);
 secondsWrap.innerHTML = '00';
 
-// single digits should have leading zero
-function padZero(num) {
-  return (num < 10) ? "0"+num : num;
-}
+startBtn.addEventListener('click', startTimer);
 
-function countDown(minutes) {
-  var int = setInterval(function () {
-    seconds--;
-
-    if(seconds % 60 === 59) {
-      minutes--;
-      minutesWrap.innerHTML = padZero(minutes);
-    }
-
-    if(seconds === 0) {
-      stopTimer();
-    }
-
-    secondsWrap.innerHTML = padZero(seconds % 60);
-  }, 1000);
-
-  var stopTimer = function() {
-    minutesWrap.innerHTML = padZero(minutes);
-    secondsWrap.innerHTML = '00';
-    clearInterval(int);
-  };
-}
-
-countDown(minutes);
